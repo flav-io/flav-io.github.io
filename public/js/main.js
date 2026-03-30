b3e2feb33d8fde2144ecce229fb109da83f08fa8
Peter Stangl <peter.stangl@ph.tum.de>
2026-03-31 01:54:33 +0200

    upgrade jquery

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function($) {
     
     /* ======= Fixed header when scrolled ======= */
     
-    $(window).bind('scroll', function() {
+    $(window).on('scroll', function() {
          if ($(window).scrollTop() > 50) {
              $('#header').addClass('navbar-fixed-top');
          }
